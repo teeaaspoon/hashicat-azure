0f33061d1c8eea66366515be6b4c5bb815f737f7
Tommy Poon <tommypoon36@gmail.com>
2022-01-06 22:12:53 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/tommy-test/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "tommypoon-workshop"
 }
